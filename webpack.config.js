6efcf76873e154faa65e707702ba6753a799a572
Michael Sickles <michael.sickles@appdynamics>
2020-04-09 16:37:02 -0400

    correct webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 		}
 	},
     entry: {
-        frontend: ['./src/index.js']
+        app: ['./src/index.js']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
